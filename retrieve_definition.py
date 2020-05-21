f02ce2c0f17c62e7b34d723414544e8b081a4719
unknown <ameralhomdy@gmail.com>
2020-05-20 20:00:40 -0700

    created flask api

--- retrieve_definition.py
+++ retrieve_definition.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Pull first 300 terms from Wikipedia article for a given term
+    Pull first 300 characters from Wikipedia article for a given term
 """
 def retrieve_definition(term):
     import requests
@@ -24,4 +24,31 @@ def retrieve_definition(term):
     R = S.get(url=URL, params=PARAMS)
     DATA = R.json()
 
-    return DATA
+    key = list(DATA['query']['pages'].keys())[0]
+    extract = DATA['query']['pages'][key]['extract']
+
+    if len(extract) == 3:
+        return open_search(term)
+
+        #exceptions function goes here
+
+    else:
+        return extract
+
+def open_search(term):
+    import requests
+    S = requests.Session()
+
+    URL = "https://en.wikipedia.org/w/api.php"
+
+    PARAMS = {
+        "action": "opensearch",
+        "search": term,
+        "redirects": "resolve",
+        "format": "json"
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    suggests = DATA[1]
+    return suggests
