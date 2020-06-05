712b264dddaed8ec72b0ca1aac18ace7c952ce61
b-whitman <53320120+b-whitman@users.noreply.github.com>
2020-06-04 23:01:50 -0400

    added gitignore file and untracked __pycache__ (#15)
    * added gitignore file and untracked __pycache__
    
    * removed .vscode
    
    Co-authored-by: Benjamin Whitman <benjamin.whitman90@gmail.com>
    Checked to ensure it included necessary file extensions

--- test_retrieve_definition.py
+++ test_retrieve_definition.py
@@ -1,5 +1,7 @@
 import unittest
+import requests
 from retrieve_definition import retrieve_definition
+
 
 class TestWikiAPI(unittest.TestCase):
     
@@ -7,6 +9,10 @@ class TestWikiAPI(unittest.TestCase):
         self.extract = retrieve_definition("cat")
         self.maxDiff = None
         self.assertEqual(self.extract, "The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter...")
+
+    def test_request(self):
+        self.request = requests.Session()
+        
 
 if __name__ == '__main__':
     unittest.main()
