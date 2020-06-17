8cf47595751fb53f9fffccd958970b068a018a05
b-whitman <53320120+b-whitman@users.noreply.github.com>
2020-06-17 00:04:28 -0400

    Refactor retrieve definition (#16)
    * isolated text_wrangle() from retrieve_definition()
    
    * small structure and doc changes
    
    * removed singularization from text_wrangle()
    
    * added exception for when opensearch() API call doesn't find any close matches
    
    * Add support for auto-singularization that works with refactored text_wrangle()
    
    * actually merged changes
    
    * fixed broken Pipfile.lock
    
    Co-authored-by: Benjamin Whitman <benjamin.whitman90@gmail.com>

--- retrieve_definition.py
+++ retrieve_definition.py
@@ -5,15 +5,18 @@
 """
 
 import requests
+import inflect
+
+# Start engine for text_wrangle() singularization
+p = inflect.engine()
 
 
 def retrieve_definition(term):
 
     S = requests.Session()
 
-    # this is the base API URL for Wikipedia
     URL = "https://en.wikipedia.org/w/api.php"
-
+    term = text_wrangle(term)
     params = {
         "action": "query",
         "prop": "extracts",
@@ -28,6 +31,7 @@ def retrieve_definition(term):
     # out Wikipedia's special formatting. Exlimit says to only return 1
     # extract.
 
+    print("Searching API for: ", term)
     response = S.get(url=URL, params=params)
     data = response.json()
     pageid = list(data['query']['pages'].keys())[0]
@@ -37,14 +41,15 @@ def retrieve_definition(term):
         # because one of the dictionary keys is the page ID for that term.
 
         if len(extract) == 3:
-            return text_wrangle(term)
+            return open_search(term)
 
         else:
             return extract
     except KeyError:
         # sometimes instead of an empty string as an extract the API call returns a "missing" key in JSON, this accounts
         # for that
-        return text_wrangle(term)
+        return open_search(term)
+
 
 
 def open_search(term):
@@ -67,34 +72,38 @@ def open_search(term):
     # Parameters set tells API to use opensearch on the given term and return the results as a JSON object.
     # Resolve means to return redirects as the page they point to.
 
-    R = S.get(url=URL, params=PARAMS)
+
+    R = S.get(url=URL, params=params)
     DATA = R.json()
     suggests = DATA[1]
-    return f"Did you mean {suggests[0]}, {suggests[1]}, {suggests[2]}?"
+    try:
+        return f"Did you mean {suggests[0]}, {suggests[1]}, {suggests[2]}?"
+    
+    except IndexError:
+        # This covers cases where input doesn't have a close Wiki entry
+        return "We can't find anything close to that :("
 
 
 def text_wrangle(term):
     """
-    Check text for various edge cases and re-run
+    Check text for various edge cases and remove
     """
-    wrangled_search = "..."
     if term.isupper():
-        wrangled_search = retrieve_definition(term.lower())
-    elif (term[0:4] == 'the ') | (term[0:4] == 'The '):
+        # Makes term lowercase
+        term = term.lower()
+        print("Lowercase search: ", term)
 
-        wrangled_search = retrieve_definition(term[4:])
-        # sends term back through function minus 'the'
-    elif term[-1:] == 's':
-        wrangled_search = retrieve_definition(term[:-1])
-        # sends terms back through function without final 's'
-    elif term[-1:] == 'e':
-        # this accounts for cases of "es" plural, the previous cycle would have
-        # removed the 's'
-        wrangled_search = retrieve_definition(term[:-1])
-    if len(wrangled_search) > 3:
-        return wrangled_search
+    if term[0:4] == 'the ':
+        # Strips 'the' and 'The' from term
+        term = term[4:]
+        print("Search without 'the': ", term)
 
-    else:
-        return open_search(term)
-        # all of the test cases have failed, function will return suggestions
-        # instead
+    if term[0:2] == 'a ':
+        term = term[2:]
+        print("Search without 'a': ", term)
+    
+    if p.singular_noun(term):
+        term = p.singular_noun(term)
+        print("Search as singular: ", term)
+
+    return term
