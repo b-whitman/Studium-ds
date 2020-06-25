29f06797fa5c32af4f702518efa8be92aebae4ae
b-whitman <53320120+b-whitman@users.noreply.github.com>
2020-06-24 22:15:51 -0400

    Test text wrangle (#24)
    * made test_retrieve_definition() hopefully more resilient
    
    * removed stub function
    
    * Made text_wrangle lowercase all incoming terms
    
    * added tests for new text_wrangle()
    
    * added additional text_wrangle() test
    
    * fixed merge conflicts
    
    Co-authored-by: Benjamin Whitman <benjamin.whitman90@gmail.com>

--- test_retrieve_definition.py
+++ test_retrieve_definition.py
@@ -1,6 +1,6 @@
 import unittest
 import requests
-from retrieve_definition import retrieve_definition
+from retrieve_definition import retrieve_definition, text_wrangle
 
 
 class TestWikiAPI(unittest.TestCase):
@@ -8,22 +8,23 @@ class TestWikiAPI(unittest.TestCase):
     def test_retrieve_definition(self):
         self.extract = retrieve_definition("cat")
         self.maxDiff = None
-        self.assertEqual(self.extract, "The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter...")
+        self.assertEqual(len(self.extract), 304)
+        
+    def test_text_wrangle(self):
+        self.term = text_wrangle("DOG")
+        self.assertEqual(self.term, "dog")
 
+        self.term = text_wrangle("the Dog")
+        self.assertEqual(self.term, "dog")
 
-def test_text_wrangle(self):
-    self.term = text_wrangle("DOG")
-    self.assertEqual(self.term, "dog")
+        self.term = text_wrangle("The Dog")
+        self.assertEqual(self.term, "dog")
 
-    self.term = text_wrangle("the Dog")
-    self.assertEqual(self.term, "dog")
+        self.term = text_wrangle("a dog")
+        self.assertEqual(self.term, "dog")
 
-    self.term = text_wrangle("a dog")
-    self.assertEqual(self.term, "dog")
-
-    self.term = text_wrangle("Dogs")
-    self.assertEqual(self.term, "dog")
-
+        self.term = text_wrangle("Dogs")
+        self.assertEqual(self.term, "dog")
 
 if __name__ == '__main__':
     unittest.main()
