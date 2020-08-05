27cec8822fd71fd1a792366f756da10519019aca
b-whitman <53320120+b-whitman@users.noreply.github.com>
2020-08-04 22:18:55 -0400

    Leitner system (#29)
    * added initial Leitner notebook
    
    * added leitner functions to module
    
    * added .gitignore; made leitner script into module; added tests
    
    * finished testing leitner.py
    
    Co-authored-by: Benjamin Whitman <benjamin.whitman90@gmail.com>

--- test_suite.py
+++ test_suite.py
@@ -1,7 +1,8 @@
 import unittest
 from retrieve_definition import text_wrangle, open_search, get_API_params, get_opensearch_params
 from autogenerate_decks import batch_search, get_article_size, get_search_string, get_params_autogen, get_params_size
-
+from leitner import leitner_dates
+import pandas as pd
 
 
 class TestAutogeneration(unittest.TestCase):
@@ -60,6 +61,32 @@ class TestWikiAPI(unittest.TestCase):
         self.assertEqual(get_opensearch_params('Cat'), {'action': 'opensearch', 'search': 'Cat', 'redirects': 'resolve',
                                                         'format': 'json'})
         self.assertIs(type(get_opensearch_params('Cat')), dict)
+
+
+class TestLeitner(unittest.TestCase):
+
+    def test_leitner_dates(self):
+        test_matrix = [(0, 0, 3, ''),
+                        (1, 0, 3, ''),
+                        (2, 1, 2, ''),
+                        (3, 1, 5, ''),
+                        (4, 0, 5, '')]
+        
+        test_df = pd.DataFrame(test_matrix, 
+            columns=['card_id', 
+                    'starred', 
+                    'comfort_level', 
+                    'next_due'])
+
+        self.df = test_df.apply(leitner_dates, axis=1)
+
+        # Expected values for comfort_level after test_df is run through
+        # leitner
+        comfort_level_series = pd.Series(data=[4,4,1,1,5], name='comfort_level')
+        pd.testing.assert_series_equal(self.df['comfort_level'], 
+                        comfort_level_series)
+        self.assertIs(type(self.df['next_due'][0]), str)
+
 
 
 if __name__ == '__main__':
