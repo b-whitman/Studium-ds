520ff73416e2f516b0fbbfbd58d4ec5e1af71d31
Amer Mahyoub <ameralhomdy97@gmail.com>
2020-08-10 19:53:26 -0700

    Updated fastapi (#34)
    * converted SVG to image Bytes
    
    * installed psutil
    
    * encoded image to base64 and change background color
    
    * fixed documentation
    
    * removed unused code
    
    * Delete gauge.png

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 from datetime import datetime
-from autogenerate_decks import autogenerate
+import json
+import pandas as pd
+from typing import List
+import uvicorn
+
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,15 +12,9 @@ from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-import calendar_heatmap
-import json
-import os
-from retrieve_definition import retrieve_definition
+from autogenerate_decks import autogenerate
 import gauge_plot
-from pydantic import BaseModel
-import pandas as pd
-from typing import List
-
+from retrieve_definition import retrieve_definition
 from leitner import leitner_dates
 
 
@@ -35,7 +34,7 @@ app.add_middleware(
 templates = Jinja2Templates(directory="templates")
 
 
-# A Pydantic model
+# A Pydantic model to parse through JSON data
 class Card(BaseModel):
     card_id : int
     isStarred : bool
@@ -67,27 +66,6 @@ async def wiki_search(word: str):
         return data
 
 
-# Create a route to return heatmap
-@app.post('/heatmap')
-async def calender_heatmap(request: Request, month: int, year: int):
-    """Return calender heatmap visual in html form"""
-    # calendar_heatmap.get_viz(month, year)
-    calendar_heatmap.get_viz()
-
-    return templates.TemplateResponse('heatmap.html', {"request": request})
-
-
-# Create route to delete heatmap
-@app.delete('/delete_heatmap')
-async def delete_heatmap():
-    """deletes heatmap html file saved in server"""
-    try:
-        os.remove('templates/heatmap.html')
-        return 'File deleted'
-    except BaseException:
-        return "File has already been deleted"
-
-
 # Create route to return gauge plot
 @app.post('/gauge')
 async def plot_gauge(request: Request, streaks: int):
@@ -100,13 +78,12 @@ async def plot_gauge(request: Request, streaks: int):
 # Create route to delete gauge plot
 @app.delete('/delete_gauge')
 async def delete_gauge():
-    """deletes gauge svg file saved in server"""
+    """deletes gauge.png file saved in server"""
     try:
-        os.remove('templates/gauge.svg')
+        os.remove('gauge.png')
         return 'File deleted'
     except BaseException:
         return "File has already been deleted"
-
 
 
 @app.get('/autogenerate_deck')
@@ -129,4 +106,4 @@ async def leitner_system(user: List[Card]):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
