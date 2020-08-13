e3240f2931fba7c2ee2417410dd83788a742b978
Cai Nowicki <cai.nowicki@gmail.com>
2020-08-12 22:19:43 -0400

    corrected test_data conversion notes, fixed variable assignment, PEP8 compliant)

--- comparative_metrics.py
+++ comparative_metrics.py
@@ -4,10 +4,6 @@ from datetime import timedelta
 
 pd.set_option('mode.chained_assignment', None)
 
-# replace this code with actual access to user data
-test_data = pd.read_csv(
-    'https://raw.githubusercontent.com/Lambda-School-Labs/Studium-ds/viz-session-quality/test_data/mean_cards_test.csv')
-
 
 def get_session_length(row):
     """ Calculate length of session in seconds"""
@@ -30,20 +26,11 @@ def convert_to_datetime(df):
     return df
 
 
-def convert_to_datetime_test_data(df):
-    """Convert datetime strings to pandas datetime object"""
-    df['session_start'] = pd.to_datetime(df['session_start'])
-    df['session_end'] = pd.to_datetime(df['session_end'])
-    return df
-
-
 def daily_cards_min_comparison(df):
     """Function to compare today's avg cards per minute to yesterday's. Returns a string consisting of the absolute value of the
     difference and the unicode for an upward (positive) or downward (negative) arrow, and a hex color code (red for
     negative, green for positive), today's average, and yesterday's average  """
-    df = convert_to_datetime_test_data(df)
-    # IMPORTANT!
-    # FIX THIS LINE BEFORE SENDING TO HEROKU!
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     yesterday = today - timedelta(days=1)
     todays_per_min = []
@@ -57,6 +44,8 @@ def daily_cards_min_comparison(df):
         if row['session_start'].date() == yesterday:
             per_min = get_cards_per_min(row)
             yesterday_per_min.append(per_min)
+    today_average = 0
+    yesterday_average = 0
     if len(todays_per_min) > 0 and len(yesterday_per_min) > 0:
         # if both days have data, then calculate the average of the list
         today_average = sum(todays_per_min) / len(todays_per_min)
@@ -85,18 +74,17 @@ def daily_cards_min_comparison(df):
         difference = abs((today_average - yesterday_average) / yesterday_average * 100)
     except ZeroDivisionError:
         # if no cards viewed yesterday, cards per min up 100% today
-        # if both averages are zero, this will display 100% = in black
+        # if both averages are zero, this will display '0 100% =' in black
         difference = 100
     s = f"{difference:.2f}% {arrow}"
-    return s, color_code, today_average,
+    return s, color_code, today_average
 
 
 def weekly_per_min_comparison(df):
     """Function to compare this week's avg cards per minute to last week's. Returns a string consisting of the absolute value of the
     difference and the unicode for an upward (positive) or downward (negative) arrow, and a hex color code (red for
     negative, green for positive), this week's average, and last week's average """
-    df = convert_to_datetime_test_data(df)
-    # FIX THIS LINE
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     this_week_start = today - timedelta(days=7)
     last_week_start = today - timedelta(days=14)
@@ -109,6 +97,8 @@ def weekly_per_min_comparison(df):
         if last_week_start <= row['session_start'].date() < this_week_start:
             per_min = get_cards_per_min(row)
             lastweek_per_min.append(per_min)
+    week_average = 0
+    lastweek_average = 0
     if len(week_per_min) > 0 and len(lastweek_per_min) > 0:
         week_average = sum(week_per_min) / len(week_per_min)
         lastweek_average = sum(lastweek_per_min) / len(lastweek_per_min)
@@ -129,6 +119,8 @@ def weekly_per_min_comparison(df):
         difference = abs((week_average - lastweek_average) / lastweek_average * 100)
     except ZeroDivisionError:
         difference = 100
+        # if no sessions last week, difference is up 100%
+        # if both averages are zero, this will display '0 100% =' in black
     s = f"{difference:.2f}% {arrow}"
     return s, color_code, week_average
 
@@ -137,8 +129,7 @@ def monthly_per_min_comparison(df):
     """Function to compare today's stats to yesterday's. Returns a string consisting of the absolute value of the
     difference and the unicode for an upward (positive) or downward (negative) arrow, and a hex color code (red for
     negative, green for positive) """
-    df = convert_to_datetime_test_data(df)
-    # FIX THIS LINE
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     this_month_start = today - timedelta(days=30)
     last_month_start = today - timedelta(days=60)
@@ -151,6 +142,8 @@ def monthly_per_min_comparison(df):
         if last_month_start <= row['session_start'].date() < this_month_start:
             per_min = get_cards_per_min(row)
             lastmonth_per_min.append(per_min)
+    month_average = 0
+    lastmonth_average = 0
     if len(month_per_min) > 0 and len(lastmonth_per_min) > 0:
         month_average = sum(month_per_min) / len(month_per_min)
         lastmonth_average = sum(lastmonth_per_min) / len(lastmonth_per_min)
@@ -171,6 +164,8 @@ def monthly_per_min_comparison(df):
         difference = abs((month_average - lastmonth_average) / lastmonth_average * 100)
     except ZeroDivisionError:
         difference = 100
+        # if no sessions last month, difference is up 100%
+        # if both averages are zero, this will display '0 100% =' in black
     s = f"{difference:.2f}% {arrow}"
     return s, color_code, month_average
 
@@ -182,8 +177,7 @@ def best_session_length(df):
         best_length = 0
         # if no data in dataframe, return 0
     else:
-        df = convert_to_datetime_test_data(df)
-        # FIX TO NON-TEST FUNCTION
+        df = convert_to_datetime(df)
         df = df.reindex(columns=df.columns.tolist() + ['cards_per_min', 'session_length'])
         # adds necessary columns to dataframe
         for index, row in df.iterrows():
@@ -200,7 +194,7 @@ def best_session_daily(df):
     """Function to determine the best session length in minutes for today and yesterday. Takes in a
     user session dataframe and returns today's best session length as an integer,
     a string of the difference in percent and the unicode for up arrow or down arrow, and a color code"""
-    df = convert_to_datetime_test_data(df)
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     yesterday = today - timedelta(days=1)
     today_card_ids = []
@@ -227,6 +221,7 @@ def best_session_daily(df):
         difference = abs((today_best_session - yesterday_best_session) / yesterday_best_session * 100)
     except ZeroDivisionError:
         # if no sessions yesterday, best session is up 100%
+        # if both best_sessions are zero, this will display '0 100% =' in black
         difference = 100
     s = f"{difference:.2f}% {arrow}"
     return today_best_session, s, color_code
@@ -236,8 +231,7 @@ def best_session_weekly(df):
     """Function to determine the best session length in minutes for this week and last week. Takes in a
     user session dataframe and returns this week's best session length as an integer,
     a string of the difference in percent and the unicode for up arrow or down arrow, and a color code"""
-    df = convert_to_datetime_test_data(df)
-    # FIX THIS LINE
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     this_week_start = today - timedelta(days=7)
     last_week_start = today - timedelta(days=14)
@@ -265,6 +259,7 @@ def best_session_weekly(df):
         difference = abs((thisweek_best_session - lastweek_best_session) / lastweek_best_session * 100)
     except ZeroDivisionError:
         # if no sessions last week, best session is up 100%
+        # if both best_sessions are zero, this will display '0 100% =' in black
         difference = 100
     s = f"{difference:.2f}% {arrow}"
     return thisweek_best_session, s, color_code
@@ -274,8 +269,7 @@ def best_session_monthly(df):
     """Function to determine the best session length in minutes for this month and last month. Takes in a
     user session dataframe and returns this month's best session length as an integer,
     a string of the difference in percent and the unicode for up arrow or down arrow, and a color code"""
-    df = convert_to_datetime_test_data(df)
-    # FIX THIS LINE
+    df = convert_to_datetime(df)
     today = datetime.date.today()
     this_month_start = today - timedelta(days=30)
     last_month_start = today - timedelta(days=60)
@@ -303,6 +297,7 @@ def best_session_monthly(df):
         difference = abs((thismonth_best_session - lastmonth_best_session) / lastmonth_best_session * 100)
     except ZeroDivisionError:
         # if last month has no sessions, the difference is up 100%
+        # if both best_sessions are zero, this will display '0 100% =' in black
         difference = 100
     s = f"{difference:.2f}% {arrow}"
     return thismonth_best_session, s, color_code
